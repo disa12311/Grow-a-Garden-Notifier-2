b09e4cab5a8dc09e374f0f775490686dce3eb5a1
disa12311 <hadiepml@gmail.com>
2025-06-17 09:28:03 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 import json
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-
 NTFY_TOPIC = os.getenv("NTFY_TOPIC")
 
-CHANNEL_IDS_STR = os.getenv("CHANNEL_IDS", "{"channelid1": "1384132746586095717", "channelid2": "1384341013895839864", "channelid3": "1384341088936005663", "channelid4": "1384341088936005663"}")
+# SỬA LỖI Ở ĐÂY: Dùng dấu ngoặc kép ba (""") để bao bọc chuỗi JSON
+CHANNEL_IDS_STR = os.getenv("CHANNEL_IDS", """{"channelid1": "1384132746586095717", "channelid2": "1384341013895839864", "channelid3": "1384341088936005663", "channelid4": "1384341088936005663"}""")
 CHANNEL_IDS = json.loads(CHANNEL_IDS_STR)
