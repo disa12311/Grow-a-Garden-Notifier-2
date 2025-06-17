1cb6dbdca521a69921c9f6d9b88b887352de6e82
disa12311 <hadiepml@gmail.com>
2025-06-17 09:22:13 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
-DISCORD_TOKEN = "your bot token id here"
+import os
+import json
 
-CHANNEL_IDS = {
-    "channelid1": "1384132746586095717",
-    "channelid2": "1384341013895839864",
-    "channelid3": "1384341088936005663",
-    "channelid4": "1384341088936005663"
-}
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
-NTFY_TOPIC = "thông báo grow-a-graden"
+NTFY_TOPIC = os.getenv("NTFY_TOPIC")
+
+CHANNEL_IDS_STR = os.getenv("CHANNEL_IDS", "{"channelid1": "1384132746586095717", "channelid2": "1384341013895839864", "channelid3": "1384341088936005663", "channelid4": "1384341088936005663"}")
+CHANNEL_IDS = json.loads(CHANNEL_IDS_STR)
