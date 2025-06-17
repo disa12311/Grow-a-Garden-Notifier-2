a56be952f6ae9ab188c7192abe6d07e23be96e8d
disa12311 <hadiepml@gmail.com>
2025-06-17 08:41:30 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ DISCORD_TOKEN = "your bot token id here"
 
 ## stock channel = both seeds and gears
 CHANNEL_IDS = {
-    channelid1: "1384132746586095717",
-    channelid2: "1384341013895839864",
-    channelid3: "1384341088936005663",
-    channelid4: "1384341088936005663"
+    "channelid1": "1384132746586095717",
+    "channelid2": "1384341013895839864",
+    "channelid3": "1384341088936005663",
+    "channelid4": "1384341088936005663"
 }
 
 NTFY_TOPIC = "thông báo grow-a-graden"
