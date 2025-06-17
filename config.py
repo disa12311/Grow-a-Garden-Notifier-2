060c8796fe20095f2c236df0cdef1bb10fcad31d
disa12311 <hadiepml@gmail.com>
2025-06-17 09:00:23 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-DISCORD_TOKEN = "your bot token id here"
+'DISCORD_TOKEN' = "your bot token id here"
 
-CHANNEL_IDS = {
+'CHANNEL_IDS' = {
     "channelid1": "1384132746586095717",
     "channelid2": "1384341013895839864",
     "channelid3": "1384341088936005663",
     "channelid4": "1384341088936005663"
 }
 
-NTFY_TOPIC = "thông báo grow-a-graden"
+'NTFY_TOPIC' = "thông báo grow-a-graden"
